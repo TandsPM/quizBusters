6cfa459a45103f752fa3d27f89a85a84ea7d7334
Tandra Malm <tandra_malm@hotmail.com>
2023-12-17 17:09:50 -0700

    added temp questions in quiz page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,19 @@
 // Client facing scripts here
+
+
+// Quizzes page link
+const express = require('express');
+const quizzesRouter = require(`./quizzes`);
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.use('/quizzes', quizzesRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
