a7abf301793cb53e2fa34141e0e7536d57f9ff09
Tandra Malm <tandra_malm@hotmail.com>
2023-12-18 11:28:35 -0700

    chnaged include to top of .ejs files

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/login/:id', (req, res) => {
+  // using encrypted cookies
+  req.session.user_id = req.params.id;
+
+  // or using plain-text cookies
+  res.cookie('user_id', req.params.id);
+
+  // send the user somewhere
+  res.redirect('/');
+});
+
 router.get('/', (req, res) => {
-  res.render('/');
+  res.send('/');
 });
 
 module.exports = router;
