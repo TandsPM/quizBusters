4aca6f0669c9c15bdad2c177f3c908f99cd56fe5
Tandra Malm <tandra_malm@hotmail.com>
2023-12-19 01:10:45 -0700

    dashboard

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,12 +1,34 @@
 $(document).ready(function () {
+  loginStatus();
+
+  // handling login button
   $('#loginButton').on('click', function (event) {
     event.preventDefault();
     window.location.href = '/index';
   });
+// });
+
+// check login status to deny access
+function loginStatus() {
+  $.ajax({
+    method: 'GET',
+    url: '/checkLogin',
+    success: function (data) {
+      if (data.user) {
+        $('#quizzesLink').attr('href', '/quizzes');
+        $('#quizzesLink').show();
+
+      } else {
+        $('#loginButton').on('click', function (event) {
+          event.preventDefault();
+          window.location.href = '/login';
+        });
+      }
+    },
+    error: function (error) {
+      console.log('Error:', error);
+    },
+  });
+}
 });
 
-// $(document).on('click', '#loginButton', function (event) {
-//   event.preventDefault();
-//   console.log('Login button ahas been clicked');
-//   window.location.href = '/';
-// })
