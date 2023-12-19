3c424f1e40b89b652d8077fd4dcdb3d307f84f7a
saintsappho <robinrosefleur@gmail.com>
2023-12-18 18:02:34 -0700

    new quiz form finally in a workable state

--- public/scripts/new-quiz.js
+++ public/scripts/new-quiz.js
@@ -1,77 +1,91 @@
 $(document).ready(function() {
 
   $(document).on('click', '.add-option-btn', addOption);
+
   $('.add-question-btn').on("click", addQuestion);
+  
   $("#new-title").on("input", updateQuizTitle);
+  
   $("#new-author").on("input", updateQuizAuthor);
-  $("#new-author").on("input", updateQuizAuthor);
+  
+  $(".new-quiz-container").on("input", ".question-input", updateQuestion);
 });
 
-let questionCounter = 1; // Start after the first question
+///////////////////////////////////////////////////////////////////////////////////////////////////
+////                      Info Functions                             ////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 const updateQuizTitle = function() {
   const inputValue = $(this).val();
+  const title = $("#quiz-title");
+  console.log("quiz-title ", title);
+  console.log("inputValue ", inputValue);
   // Update the content of the new-quiz-title h1 element
-  $("#new-quiz-title").text(inputValue);
+  title.text(inputValue);
 };
 
 const updateQuizAuthor = function() {
-  const inputValue = $("#new-author-form").val();
+  const inputValue = $(this).val();
   // Update the content of the new-quiz-autho h1 element
-  $("#new-quiz-author").text(inputValue);
+  $("#quiz-author").text(inputValue);
 };
 
-const updateQuestion1 = function() { // NEED TO MAKE UPDATEQUESTION*
-  const inputValue = $(".new-question").val();
-  // Update the content of the new-question-1 h1 element
-  $d("#new-question-1").text(inputValue);
-};
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+////                      Option Functions                             ////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 const addOption = function(event) {
   event.preventDefault(); // Prevent default form submit
-  const optionsBox = $(this).siblings("div").last(); // Change to class of your question options
-  const optionCounter = optionsBox.find('.new-question-option').length + 1; // Count existing options
-  console.log("optionsBox.find", optionsBox.find('.new-question-option'))
-
+  const optionsBox = $(this).siblings("div").last(); // Change class to this question's options box
+  
+  console.log("optionsBox.find", optionsBox.find('.new-question-option'));
+  
   // Create a new option input
   const newOptionHtml = `
-    <div class="new-question-option">
-    <input type="text" name="option-${optionCounter}" value="option-${optionCounter}" />
-    </div>
-`;
+  <div class="new-question-option">
+  <input type="text" name="option" value="[option]" />
+  </div>
+  `;
   // Append the new option input to the correct question's options box
   optionsBox.append(newOptionHtml);
 };
 
-const optionCounter = function() {
-  
-}
+///////////////////////////////////////////////////////////////////////////////////////////////////
+////                      Question Functions                             ////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+const updateQuestion = function() {
+  const input = $(this);
+  const text = input.val();
+  const div = input.closest("div");
+  const questionName = div.find(".question-name");
+  questionName.text(text);
+};
 
 const addQuestion = function() {
   event.preventDefault();
-  // Increment the question counter for unique question-ids
-  questionCounter++;
-
-  // Create a new question form
-  const newQuestionHtml = `
-   <form>
-    <label for="new-question-form-${questionCounter}">
-    Question ${questionCounter}:</label><br>
-      <input type="text" class="new-question-form-${questionCounter}" name="new-question-form-${questionCounter}" value="Whats up?" onkeyup="updateQuestion${questionCounter}()"><br>
-    </form>
-    <h4 class="new-question-${questionCounter}">What's up?</h4>
-    <div class="question-${questionCounter}-options">
-    <fieldset>
-      <div class="new-question-options-box">
-        <div class="new-question-option"><input type="text" name="option-1" value="option-1" /></div>
-        <div class="new-question-option"><input type="text" name="option-2" value="option-2" /></div>
-        <button class="add-option-btn">Add Another option</button>
-        </div>
-        </fieldset>
-        <br>
-        </div>`;
-
+  const element = createQuestionElement();
   // Append the new question HTML to the "new-questions" section
-  $('.new-quiz-questions').append(newQuestionHtml);
+  $('.new-quiz-questions').append(element);
+};
+
+const createQuestionElement = function() {
+  const newQuestionHtml = `
+  <div class="individual-question">
+    <label for="new-question">New Question:</label><br>
+    <input type="text" class="question-input" name="question-input" value="What colour is the sky?"><br>
+    <h4 class="question-name">What colour is the sky?</h4> 
+    <div class="question-options">
+      <fieldset>
+        <div class="new-question-options-box"> <!-- add new options to here-->
+          <div class="new-question-option"><input type="text" name="option" value="Blue" /></div>
+          <div class="new-question-option"><input type="text" name="option" value="Green" /></div>
+        </div>
+        <button type="button" class="add-option-btn">Add Another Option</button>
+      </fieldset>
+    </div>
+  </div>
+  <br>`;
+  return $(newQuestionHtml);
 };
