cb477b528812d31df1137b4cc3324713bbd04ba6
Tandra Malm <tandra_malm@hotmail.com>
2023-12-20 01:09:06 -0700

    trying to fix merge issue

--- routes/index.js
+++ routes/index.js
@@ -6,61 +6,105 @@ const router = express.Router();
 const cookieSession = require('cookie-session');
 const db = require('../db/connection');
 const { getUserById } = require('../db/queries/users');
-const { Pool } = require('pg');
 
-const db = require('../db/connection');
+router.use(cookieSession({
+  secret: 'your-secret-key',
+  resave: true,
+  initialSession: true,
+}));
 
-
+//////////////////////////////////////////////////////////////////////
+//// Check if Logged in                                           ////
+//////////////////////////////////////////////////////////////////////
 const allowAccess = (req, res, next) => {
-  if (req.session && (req.session.user)) {
+  if (req.session.user) {
     next();
   } else {
     res.redirect('/login');
   }
 };
 
-// const quizzes = [
-//   { id: 1, title: 'Quiz 1', rating: 5 },
-//   { id: 2, title: 'Quiz 2', rating: 4 },
-// ];
-
-// execute a SELECT query to retrieve all rows from the 'quizzes' table
-// const getQuizzesFromDatabase = async () => {
-//   try {
-//     const result = await db.query('SELECT * FROM quizzes');
-//     return result.rows;
-//   } catch (error) {
-//     console.error('Error fetching quizzes from the database:', error);
-//     throw error;
-//   }
-// };
-
-// router.get('/index', allowAccess, async (req, res) => {
-//   try {
-//     const quizzes = await getQuizzesFromDatabase();
-//     res.render('index', { user_id: req.session.user, quizzes: quizzes });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
+const quizzes = [
+  { id: 1, title: 'Quiz 1', rating: 5 },
+  { id: 2, title: 'Quiz 2', rating: 4 },
+];
 
 router.get('/login/:id', (req, res) => {
-  req.session.user_id = req.params.id;
+
+  req.session.user_id = req.body.id;
   res.redirect('/');
 });
 
+//////////////////////////////////////////////////////////////////////
+//// render login page                                            ////
+//////////////////////////////////////////////////////////////////////
 router.get('/', allowAccess, (req, res) => {
+
   const user = req.session.user;
   res.render('dashboard', { user: user });
 });
 
-// router.get('/index', allowAccess, async (req, res) => {
-//   res.render('index', { user_id: req.session.user, quizzes: quizzes });
-// });
+router.get('/index', allowAccess, async (req, res) => {
+  res.render('index', { user_id: req.session.user, quizzes: quizzes });
+});
+
+router.get('/dashboard', allowAccess, (req, res) => {
+
+  const user = req.session.user;
+  res.render('dashboard', { user: user });
+});
+
+//////////////////////////////////////////////////////////////////////
+//// post login by user id                                        ////
+//////////////////////////////////////////////////////////////////////
+
+router.post('/login', (req, res) => {
+  const id = req.body.id;
+  const user = getUserById(id);
+
+  if (user) {
+    req.session.user_id = id;
+    req.session.user = user;
+    // redirect to the home page after login
+    res.redirect('/');
+  } else {
+    // Handle invalid user ID
+    res.status(404).send('User not found');
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.user) {
+    res.redirect('/');
+  } else {
+  res.render('login');
+  }
+});
+
+// Add a route for handling login form submission
+router.post('/login', (req, res) => {
+  const id = req.body.id;
+  const user = getUserById(id);
+
+  if (user) {
+    req.body.user = user;
+    const redirect = req.query.redirect || '/';
+
+    res.redirect(redirect);
+  } else {
+    res.status(404).send('User not found');
+  }
+});
+
+
+//////////////////////////////////////////////////////////////////////
+//// Endpoint to check login status                               ////
+//////////////////////////////////////////////////////////////////////
 
 router.get('/checkLogin', (req, res) => {
-  const user = req.session.user;
+
+  console.log("req.session3: ", req.session);
+  const user = req.body.user;
   res.json({ user });
 });
 
